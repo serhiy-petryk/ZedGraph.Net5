32a5b17dcd8a366bfdb7c3efd8b7012dbac57567
serhiy-petryk <sergei_petrik@yahoo.com>
2023-06-21 03:22:00 +0300

    First commit

---
+++ ZedGraph/TestWinForms/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace TestWinForms
+{
+  partial class Form1
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+      this.SuspendLayout();
+      // 
+      // zedGraphControl1
+      // 
+      this.zedGraphControl1.Location = new System.Drawing.Point(148, 98);
+      this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+      this.zedGraphControl1.Name = "zedGraphControl1";
+      this.zedGraphControl1.ScrollGrace = 0D;
+      this.zedGraphControl1.ScrollMaxX = 0D;
+      this.zedGraphControl1.ScrollMaxY = 0D;
+      this.zedGraphControl1.ScrollMaxY2 = 0D;
+      this.zedGraphControl1.ScrollMinX = 0D;
+      this.zedGraphControl1.ScrollMinY = 0D;
+      this.zedGraphControl1.ScrollMinY2 = 0D;
+      this.zedGraphControl1.Size = new System.Drawing.Size(422, 173);
+      this.zedGraphControl1.TabIndex = 0;
+      this.zedGraphControl1.UseExtendedPrintDialog = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.zedGraphControl1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private ZedGraph.ZedGraphControl zedGraphControl1;
+  }
+}
+
